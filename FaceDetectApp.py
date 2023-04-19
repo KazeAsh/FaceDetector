143a87a04db752c4867cb23e87da52003caf2e95
Ashley Pilger <111095466+KazeAsh@users.noreply.github.com>
2023-04-20 00:24:58 +0900

    Update FaceDetectApp.py
    Revised

--- FaceDetectApp.py
+++ FaceDetectApp.py
@@ -19,16 +19,22 @@ while True:
   
   #must convert to grayscale
   grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
   #Detect Faces
   face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
   #draw rectangles to show detection on the face_coordinates
-  for (x,y,w,h) in face_coordinates:
-    cv2.rectangle(frame, (x,y), (x+w, y+h), randrange(256), randrange(256), randrange(256), 4)
-    cv2.imshow('Clever Programmer Face Detector', frame)
-  key = cv2.waitkey(1)
-#stops if you hit Q
+  for (x, y, w, h) in face_coordinates:
+      cv2.rectangle(frame, (x, y), (x+w, y+h), randrange(256), randrange(256), randrange(256), 9)
+    
+  cv2.imshow('Clever Programmer Face Detector', frame)
+  key = cv2.waitKey(1)
+    
+    #stops if you hit Q
   if key==81 or key==113:
     break
 
 #Releases the VideoCapture object
 webcam.release()
+
+print("Code Completed")
